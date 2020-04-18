696ef0d5bdc3286945d0cd5ab5c5764a1c599b95
飯田恭子 <iidakyouko@mba.local>
2020-04-18 17:28:23 +0900

    [Update task_2_Bookers]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,7 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
+      flash[:notice]="Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books=Book.all
@@ -25,6 +26,7 @@ class BooksController < ApplicationController
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice]="Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render 'books/edit'
@@ -34,10 +36,13 @@ class BooksController < ApplicationController
   def destroy
     @book=Book.find(params[:id])
     @book.destroy
+    flash[:notice]="Book was successfully destroyed."
     redirect_to books_path
   end
 
+
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
